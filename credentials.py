52104ebac50b8900e64031863d67d7d52eea375e
Janashruti <janashruti333@gmail.com>
2025-05-28 12:13:55 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,11 +1,11 @@
 # api details.py
 import os
 
-SHOPIFY_API_KEY = os.getenv("47f17fe93884243d001fe7f9045f64e2")
-SHOPIFY_PASSWORD = os.getenv("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")
-SHOPIFY_STORE = os.getenv("erode.clothing")
+SHOPIFY_API_KEY = "xxxxxxxxxxxx"
+SHOPIFY_PASSWORD = "xxxxxxxxxxxx"
+SHOPIFY_STORE = "xxxxxxxxxxxx"
 
-EMAIL_ADDRESS = os.getenv("janashruti333@gmail.com")  # for sending report
-EMAIL_PASSWORD = os.getenv("siyunqqtsxvmmtpa")
+EMAIL_ADDRESS = "abc@gmail.com"  # for sending report
+EMAIL_PASSWORD = "xxxxxxxxx"
 
-MANAGER_EMAIL = os.getenv("vgfgg113@gmail.com")  # for receiving report
+MANAGER_EMAIL = "xxxxxxxxxx"  # for receiving report
